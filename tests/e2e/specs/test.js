6c3521f91c2892e83b790c453edde1d963148900
mjmccoach <mmccoach88@gmail.com>
2021-01-06 15:05:30 +0000

    Written and tested should have number buttons that update display of running total and should be able to have the operator buttons update the display of running total

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -9,4 +9,20 @@ describe('calculator', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should have number buttons that update display of running total', () => {
+		cy.get('#number3').click();
+		cy.get('#number4').click();
+		cy.get('#number5').click();
+		cy.get('.display').should('contain', '345');
+  });
+  
+  it('should be able to have the operator buttons update the display of running total', () => {
+    cy.get('#operator_add').click(); 
+    cy.get('#number5').click();
+    cy.get('#operator_subtract').click();
+    cy.get('#number3').click();
+    cy.get('#operator_equals').click();
+    cy.get('.display').should('contain', '2');
+  });
 })
