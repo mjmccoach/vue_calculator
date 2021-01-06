ec108822bcc9fb4e23fd20f7aa02422a2dbcf507
mjmccoach <mmccoach88@gmail.com>
2021-01-06 14:23:57 +0000

    Written and tested it should be able to concatenate multiple numbere clicks

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -31,5 +31,20 @@ describe('App.vue', () => {
     wrapper.vm.multiply('5');
     expect(wrapper.vm.runningTotal).to.equal(15)
   })
-  
+
+  it('should be able to divide twenty-one by seven and get three', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 21;
+    wrapper.vm.divide('7');
+    expect(wrapper.vm.runningTotal).to.equal(3)
+  })
+
+  it('should be able to concatenate multiple number clicks', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.numberClick('5');
+    wrapper.vm.numberClick('6');
+    wrapper.vm.numberClick('7');
+    wrapper.vm.numberClick('8');
+    expect(wrapper.vm.runningTotal).to.equal(5678)
+  })
 })
