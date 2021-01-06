75bf181353582bffcb4b0744927a43fdee07db7e
mjmccoach <mmccoach88@gmail.com>
2021-01-06 14:51:22 +0000

    Written and tested it should be able to clear the running total without affecting the calculation

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -62,4 +62,17 @@ describe('App.vue', () => {
     wrapper.vm.operatorClick('=');
     expect(wrapper.vm.runningTotal).to.equal(3)
   })
+
+  it('should be able to clear the running total without affecting the calculation', () => {
+    const wrapper = shallowMount(App)
+    
+    wrapper.vm.operatorClick('+');
+    wrapper.vm.numberClick('5')
+    wrapper.vm.operatorClick('-')
+    wrapper.vm.numberClick('1')
+    wrapper.vm.operatorClick('=')
+    wrapper.vm.clearClick();
+
+    expect(wrapper.vm.previousTotal).to.equal(4)
+  })
 })
